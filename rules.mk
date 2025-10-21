d42d557c82ee7fee376fe1b87e76b1973c85ce51
iscipher <github@ioconfig.com>
2025-10-20 23:54:27 -0400

    updating rules.mk

--- rules.mk
+++ rules.mk
@@ -1,1 +1,18 @@
-CAPS_WORD_ENABLE = yes
+BOOTMAGIC_ENABLE = yes      # Enable bootmagic lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = no          # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+NKRO_ENABLE = no            # N-Key Rollover
+CAPS_WORD_ENABLE = yes       # Caps word feature
+
+# Split keyboard
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor       # Required for RP2040 serial communication
+
+# RGB Lighting
+RGBLIGHT_ENABLE = yes        # Enable RGB underglow (you have 12 LEDs defined)
+
+# Audio and RGB Matrix (not needed for your build)
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no    # Use RGBLIGHT instead
